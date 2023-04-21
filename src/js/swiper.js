f4ea6b3afc2755cd95c266be858a6594c44abf43
UnaRumel <108756938+UnaRumel@users.noreply.github.com>
2023-04-21 21:34:17 +0300

    1

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,5 +1,17 @@
-var swiper = new Swiper('.mySwiper', {
+import Swiper, {Pagination, Autoplay} from "swiper";
+
+const swiper = new Swiper('.swiper', {
+  modules: [Pagination, Autoplay],
+  direction: 'horizontal',
+  loop: true,
+
   pagination: {
     el: '.swiper-pagination',
+    // Добавил кликабельность для аксессуаров
+    clickable: true,
+  },
+
+  autoplay: {
+    delay: 3000,
   },
 });
